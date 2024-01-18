8e64dca065a930e14ab0c4d818024f775114850b
Kynix Chan <kynixcn@gmail.com>
2024-01-18 16:16:25 +0800

    Update flake.nix

--- cuda-py/flake.nix
+++ cuda-py/flake.nix
@@ -1,35 +1,31 @@
 {
-  description = "My Flake";
+      description = "Python 3 development environment";
+      outputs = { self, nixpkgs }:
+      let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          config.cudaSupport = true;
+          config.cudaVersion = "12";
+        };
+      in {
+        devShells.${system}.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cudatoolkit linuxPackages.nvidia_x11
+            cudaPackages.cudnn
+            libGLU libGL
+            xorg.libXi xorg.libXmu freeglut
+            xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib 
+            ncurses5 stdenv.cc binutils
+            (pkgs.python3.withPackages(ps: with ps; [
+              pip
+            ]))
+          ];
 
-  inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
-  };
-
-  outputs = { self, nixpkgs }: 
-    let
-      pythonEnv = nixpkgs.python3.withPackages (ps: with ps; [ pip ]);
-    in
-    {
-      devShell = nixpkgs.mkShell {
-        buildInputs = with nixpkgs; [
-          pythonEnv
-          git gitRepo gnupg autoconf curl
-          procps gnumake util-linux m4 gperf unzip
-          cudatoolkit linuxPackages.nvidia_x11
-          libGLU libGL
-          xorg.libXi xorg.libXmu freeglut
-          xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib 
-          ncurses5 stdenv.cc binutils
-        ];
-        shellHook = ''
-          export CUDA_PATH=${nixpkgs.cudatoolkit}
-          export LD_LIBRARY_PATH=${nixpkgs.linuxPackages.nvidia_x11}/lib:${nixpkgs.ncurses5}/lib
-          export EXTRA_LDFLAGS="-L/lib -L${nixpkgs.linuxPackages.nvidia_x11}/lib"
-          export EXTRA_CCFLAGS="-I/usr/include"
-
-          # 可选：激活Python虚拟环境
-          source ${pythonEnv}/bin/activate
-        '';
+          shellHook = ''
+              export LD_LIBRARY_PATH="${pkgs.linuxPackages.nvidia_x11}/lib"
+          '';          
+        };
       };
-    };
-}
+    }
